6b216e58e9a3a1323e00e7b2f486ad6bfdea8c95
Yousefalm1 <yousefalm@outlook.com>
2024-03-12 21:56:31 +0300

    Added product model, product and custom admin panel

---
+++ products/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Product
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'name',
+        'description',
+        'price',
+        'image',
+    )
+
+    ordering =('product_id',)
+
+admin.site.register(Product, ProductAdmin)
