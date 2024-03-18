930761562a9910b807eea40fb504fff03a3cd0f2
Yousefalm1 <yousefalm@outlook.com>
2024-03-18 22:43:04 +0300

    Added three new fields to the product model

--- products/models.py
+++ products/models.py
@@ -7,13 +7,17 @@ class Product(models.Model):
 
     name = models.CharField(max_length=254)
     description = models.TextField()
+    product_specifications = models.TextField()
+    availability_shipping_info = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True) 
     image = models.ImageField(null=True, blank=True)
     product_id = models.AutoField(primary_key=True)
+    quantity = models.PositiveIntegerField(default=0)
+
     # 'celeb_profile.CelebProfile' this is the app and where the model is 
     celeb_profile = models.ForeignKey('celeb_profile.CelebProfile', on_delete=models.CASCADE, related_name='products', blank=True, null=True)
-    
+
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -8,7 +8,9 @@ from celeb_profile.models import CelebRequest
 def all_products(request):
     """ A view to show all products """
 
+    # For nav
     has_approved_request = CelebRequest.objects.filter(user=request.user, approved=True).exists()
+    # For nav
 
     products = Product.objects.all()
 
@@ -23,14 +25,22 @@ def all_products(request):
 def product_detail(request, product_id):
     """ A view to show individual product details """
 
-    has_approved_request = CelebRequest.objects.filter(user=request.user, approved=True).exists()
 
+    # For nav
+    has_approved_request = CelebRequest.objects.filter(user=request.user, approved=True).exists()
+    # For nav
 
     product = get_object_or_404(Product, pk=product_id)
 
+
+    # Product.objects.filter used to retrieve products from database
+    # celeb_profile=product.celeb_profile filters products where the celeb_profile 
+    celeb_profile_products = Product.objects.filter(celeb_profile=product.celeb_profile)
+
     context = {
         'product': product,
-        'has_approved_request': has_approved_request
+        'has_approved_request': has_approved_request,
+        'celeb_profile_products': celeb_profile_products,
     }
 
     return render(request, 'products/product_detail.html', context)
