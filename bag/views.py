c710f74731580a3a2b855e7b0239bd90b63e45b6
Yousefalm1 <yousefalm@outlook.com>
2024-03-20 23:11:28 +0300

    Added bag context and base delivery logic

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
 
 # Create your views here.
@@ -6,4 +6,18 @@ from products.models import Product
 def view_bag(request):
     """ A view that renders the contents in the users shopping bag """
 
-    return render(request, 'bag/bag.html')
+    return render(request, 'bag/bag.html', {})
+
+
+
+def add_bag(request):
+    pass
+
+
+
+
+def delete_bag(request):
+    pass
+
+def update_bag(request):
+    pass
