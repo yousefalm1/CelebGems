23a249e848a3789452cc90ea65b1f5d9f0728c1c
Yousefalm1 <yousefalm@outlook.com>
2024-03-21 22:17:08 +0300

    Added has sizes to product

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     description = models.TextField()
     product_specifications = models.TextField()
     availability_shipping_info = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True) 
     image = models.ImageField(null=True, blank=True)
