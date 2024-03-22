661660dcf9da3b0b22ba7f36835b42a61241323f
Yousefalm1 <yousefalm@outlook.com>
2024-03-22 20:54:42 +0300

    Added and adjusted add_to_bag view to work with sizes and quantity

--- bag/views.py
+++ bag/views.py
@@ -15,12 +15,27 @@ def add_to_bag(request, product_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
     bag = request.session.get('bag', {})
 
-    if product_id in list(bag.keys()):
-        bag[product_id] += quantity
+    if size:
+        if product_id in list(bag.keys()):
+            if size in bag[product_id]['products_by_size'].keys():
+                bag[product_id]['products_by_size'][size] += quantity
+            else:
+                bag[product_id]['products_by size'][size] = quantity
+        else:
+            bag[product_id] = {'products_by_size': {size: quantity }}
+
     else:
-        bag[product_id] = quantity
+        if product_id in list(bag.keys()):
+            bag[product_id] += quantity
+        else:
+            bag[product_id] = quantity
+
+
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
