e758c3415d01b18d93d1105e7edf7626628a9508
Yousefalm1 <yousefalm@outlook.com>
2024-03-23 23:29:12 +0300

    Create includes for javascript and add quanity btn javascript functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,21 +5,45 @@ from products.models import Product
 
 def bag_contents(request):
 
+    # Creates an empty list to store info about the products in the shopping bag
     bag_products = []
+    # Creates a variable to keep track of the total cost of all the products in the shopping bag
     total = 0
+    # Creates a variable to count the total number of products in the bag
     product_count = 0
-    # Use a different variable name to retrieve the bag from the session
-    bag = request.session.get('bag', {})  # Use 'bag' instead of bag
+    # Get the shopping bag from the session if no bag it creates a dict 
+    bag = request.session.get('bag', {})  
 
-    for product_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=product_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_products.append({
-            'product_id': product_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    # loops thru each bag item in bag using bag.items
+    for product_id, product_data in bag.items():
+        # Checks if product_data is an integer (which means the product has no size variations)
+        if isinstance(product_data, int):
+            # Gets the corresponding product from the database 
+            product = get_object_or_404(Product, pk=product_id)
+            # Calculates the total cost of all products in bag and updates the product count 
+            total += product_data * product.price
+            product_count += product_data
+            # appends the info to the bag_products list
+            bag_products.append({
+                'product_id': product_id,
+                'quantity': product_data,
+                'product': product,
+            })
+        else:
+            # if products data is not a integer meaning it has sizes 
+            product = get_object_or_404(Product, pk=product_id)
+            # it loops thru each size and quantity in the product data 'products_by_size
+            for size, quantity in product_data['products_by_size'].items():
+                # calculates the total cost and updates the product count 
+                total += quantity * product.price
+                product_count += quantity
+                # appends the info about product and size to bag_products list 
+                bag_products.append({
+                    'product_id': product_id,
+                    'quantity': product_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -30,6 +54,7 @@ def bag_contents(request):
     
     grand_total = delivery + total
     
+    # The whole project can use this since its added to settings
     context = {
         'bag_products': bag_products,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -13,31 +13,47 @@ def view_bag(request):
 def add_to_bag(request, product_id):
     """ Add a quantity of the product to the shopping bag """
 
+
+    # Gets the quantity of the product the redirect_ulr and sets the size to None if there is a product size it get the product size
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
-        size = request.POST['size']
+        size = request.POST['product_size']
+    # this retrieves the current bag of the session if no bag it it sets up an empty dict
     bag = request.session.get('bag', {})
 
+    # Checks if size is selected for product
     if size:
+        # then checks if product is already in bag
         if product_id in list(bag.keys()):
+            # checks if the selected size for the product is already present in bag if so it adds to the quantity
+            #  returns a view object containing all the keys (sizes) present in the products_by_size dictionary for the specific product identified by product_id.
             if size in bag[product_id]['products_by_size'].keys():
                 bag[product_id]['products_by_size'][size] += quantity
             else:
-                bag[product_id]['products_by size'][size] = quantity
+                # if that size is not there but the product is there it creates a new line entry in the 'products_by_size' dict 
+                bag[product_id]['products_by_size'][size] = quantity
         else:
+            # if no  product in bag but has size
             bag[product_id] = {'products_by_size': {size: quantity }}
 
+    # If the product has no size this code block is executed 
     else:
+        # bag.keys() gets all the productIds(keys) from the bag dict
+        # list(bag.keys()) converts these keys into a list
+        # product_id in list(bag.keys()) checks if the product_id is present in this list of keys.
         if product_id in list(bag.keys()):
+            # if product is in the bag it updated the quantity 
             bag[product_id] += quantity
         else:
+            # or else it will add the the product along with the quantity to the bag
             bag[product_id] = quantity
 
-
+    # Updates the bag data with the modified bag dict
     request.session['bag'] = bag
     print(request.session['bag'])
+    # Redirects the user to a url after adding the product
     return redirect(redirect_url)
 
 
