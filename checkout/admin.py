1797ad06f38f7d7aecb3224127ed17f2e75f1487
Yousefalm1 <yousefalm@outlook.com>
2024-03-28 22:30:16 +0300

    Added the update_on_save funciton to the signals.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,30 +3,30 @@ from .models import Order, OrderLineItem
 # Register your models here.
 
 # tabularinline allows to display and edit related objects on the same page as the parent object in a table format
-class OrderAdminOrderLineItem(admin.TabularInline):
+class OrderLineItemAdminInline(admin.TabularInline):
     # this will make it so we dont have to go to the OrderLineItem interface to edit we can do it thru thew order interface
-    
     model = OrderLineItem
-
     readonly_fields = ('lineitem_total',)
-
-
-
-
 
 class OrderAdmin(admin.ModelAdmin):
     # Add to the order admin interface
-    inlines = (OrderAdminOrderLineItem)
+    inlines = (OrderLineItemAdminInline,)
 
     # specifies the fields in the admin interface that should be displayed as read-only,
-    readonly_fields = ('order_number', 'data', 'delivery_cost', 'order_total', 'grand_total',)
-
+    readonly_fields = ('order_number', 'date',
+                    'delivery_cost', 'order_total',
+                    'grand_total',)
     # specifies the order in which the fields should appear in the admin interface
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county', 'delivery_cost', 'order_total','grand_total',)
-
+    fields = ('order_number', 'date', 'full_name',
+            'email', 'phone_number', 'country',
+            'postcode', 'town_or_city', 'street_address1',
+            'street_address2', 'county', 'delivery_cost',
+            'order_total', 'grand_total',)
     # specifies which fields should be displayed as columns in the list view of orders in the admin interface
-    list_display = ('order_number', 'date', 'full_name','order_total', 'delivery_cost',  'grand_total',)
-
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+    
     ordering = ('-date',)
 
 
---
+++ checkout/signals.py
@@ -0,0 +1,16 @@
+# this means that after a model instance is saved and deleted it is sent by django to the entire applications 
+from django.db.models.signals import post_save, post_delete
+# To receive these signals
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+# To Execute this function whenever the post save signal is sent 
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+
+    # Access the order this specific item is related to and call the update_total method
+    instance.order.update_total()
