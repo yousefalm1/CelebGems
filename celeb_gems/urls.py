2bc6f9229015d5862be55f3518002cf5f738726c
Yousefalm1 <yousefalm@outlook.com>
2024-03-29 21:28:19 +0300

    Add url to project url

--- celeb_gems/urls.py
+++ celeb_gems/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('celeb/', include('celeb_profile.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
