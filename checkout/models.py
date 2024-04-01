88270e85719d56867c68b0d828cdff3087273b0e
Yousefalm1 <yousefalm@outlook.com>
2024-04-01 23:13:22 +0300

    Add import country field to model

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
 from products.models import Product
 # Create your models here.
 
