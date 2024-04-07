6c7fe0a60542faf4f00feda2a5c7d1d127b5217f
Yousefalm1 <yousefalm@outlook.com>
2024-04-07 21:16:22 +0300

    Addded products to home views

--- home/views.py
+++ home/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from celeb_profile.models import CelebRequest, CelebProfile
+from products.models import Product
 
 def index(request):
     """ A view to return the index page """
 
     
     celeb_profiles = CelebProfile.objects.filter(display_on_home=True)
+    products = Product.objects.filter(display_on_home=True)
 
     # default value if user is not authenticated  
     has_approved_request = False
@@ -20,6 +22,7 @@ def index(request):
     context = {
         'celeb_profiles': celeb_profiles,
         'has_approved_request': has_approved_request,
+        'products': products,
     }
 
 
