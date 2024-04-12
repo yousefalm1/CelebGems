61ddd8e5c17e210871575720b6f234d2f1bb4669
Yousefalm1 <yousefalm@outlook.com>
2024-04-12 15:53:54 +0300

    update settings

--- celeb_gems/settings.py
+++ celeb_gems/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*', 'https://celebgems.onrender.com' ]
 
 
 INSTALLED_APPS = [
