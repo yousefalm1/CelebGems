360b151c02d0a41e867636313e45829f45e3adf9
Yousefalm1 <yousefalm@outlook.com>
2024-04-12 19:50:30 +0300

    fix Url

--- celeb_gems/urls.py
+++ celeb_gems/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +15,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls') )
 
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
