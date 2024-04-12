4fbe3a82c0cef716a222167d167b1deb4fdb4203
Yousefalm1 <yousefalm@outlook.com>
2024-04-12 19:47:38 +0300

    fix cloudinary

--- celeb_gems/urls.py
+++ celeb_gems/urls.py
@@ -1,8 +1,6 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +12,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls') )
 
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
+]
