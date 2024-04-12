e463077596f5451b8e05a8d03009bd461b8dddc9
Yousefalm1 <yousefalm@outlook.com>
2024-04-12 19:54:56 +0300

    Add staitc to urls

--- celeb_gems/urls.py
+++ celeb_gems/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls') )
 
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
