4efc39bec66957d992d598232aec301451b9af54
Yousefalm1 <yousefalm@outlook.com>
2024-04-13 12:23:29 +0300

    Fix heroku

--- celeb_gems/settings.py
+++ celeb_gems/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://celeb-gems-99c19655d59c.herokuapp.com', '*']
+ALLOWED_HOSTS = ['celeb-gems-99c19655d59c.herokuapp.com', '*']
 
 
 INSTALLED_APPS = [
@@ -138,13 +138,20 @@ USE_L10N = True
 USE_TZ = True
 
 
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
