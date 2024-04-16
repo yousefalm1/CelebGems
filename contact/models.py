869fa5d131387a93bacd106b97406f2270b7f20c
Yousefalm1 <yousefalm@outlook.com>
2024-04-16 20:35:05 +0300

    Create model for contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class contactMessage(models.Model):
+    Name = models.CharField(max_length= 100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.subject
