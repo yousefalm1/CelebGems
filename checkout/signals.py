e71a4b01c615fd92c2188733acb477420eb80e5b
Yousefalm1 <yousefalm@outlook.com>
2024-04-18 19:12:59 +0300

    Add order detial in checkout success page

--- checkout/signals.py
+++ checkout/signals.py
@@ -7,6 +7,7 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
+
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
