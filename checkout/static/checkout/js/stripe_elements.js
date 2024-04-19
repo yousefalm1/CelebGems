aad285f8e7dcf2f59d754e349d9b609627a7c399
Yousefalm1 <yousefalm@outlook.com>
2024-04-19 14:45:59 +0300

    Add loading orange/black overlay for stripe

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -34,12 +34,16 @@ card.addEventListener("change", function (event) {
     errorDiv.textContent = "";
   }
 });
+
+// form submit
 var form = document.getElementById("payment-form");
 
 form.addEventListener("submit", function (ev) {
   ev.preventDefault();
   card.update({ disabled: true });
   $("#submit-button").attr("disabled", true);
+  $("#payment-form").fadeToggle(100);
+  $("#loading-overlay").fadeToggle(100);
   stripe
     .confirmCardPayment(clientSecret, {
       payment_method: {
@@ -57,6 +61,8 @@ form.addEventListener("submit", function (ev) {
         <span>${result.error.message}</span>
       `;
         $(errorDiv).html(html);
+        $("#payment-form").fadeToggle(100);
+        $("#loading-overlay").fadeToggle(100);
         card.update({ disabled: false });
         $("#submit-button").attr("disabled", false);
       } else {
