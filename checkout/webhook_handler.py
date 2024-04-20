2c10033e4ba46b702ecdc72d69ff91972f03e6af
Yousefalm1 <yousefalm@outlook.com>
2024-04-20 19:44:04 +0300

    Fix celeb page to make them all the same size

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+import stripe
 
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -58,14 +59,14 @@ class StripeWH_handler:
             try:
                 order = Order.object.get(
                     full_name__iexact=shipping_details.name,
-                    email__iexact=shipping_details.email,
+                    email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.country,
-                    postcode__iexact=shipping_details.postal_code,
-                    town_or_city__iexact=shipping_details.city,
-                    street_address1__iexact=shipping_details.line1,
-                    street_address2__iexact=shipping_details.line2,
-                    county__iexact=shipping_details.state,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
@@ -85,14 +86,14 @@ class StripeWH_handler:
             try:
                 order = Order.object.create(
                         full_name=shipping_details.name,
-                        email=shipping_details.email,
+                        email=billing_details.email,
                         phone_number=shipping_details.phone,
-                        country=shipping_details.country,
-                        postcode=shipping_details.postal_code,
-                        town_or_city=shipping_details.city,
-                        street_address1=shipping_details.line1,
-                        street_address2=shipping_details.line2,
-                        county=shipping_details.state,
+                        country=shipping_details.address.country,
+                        postcode=shipping_details.address.postal_code,
+                        town_or_city=shipping_details.address.city,
+                        street_address1=shipping_details.address.line1,
+                        street_address2=shipping_details.address.line2,
+                        county=shipping_details.address.state,
                         original_bag=bag,
                         stripe_pid=pid,
 
--- profiles/models.py
+++ profiles/models.py
@@ -5,6 +5,8 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
+
+
 class UserProfile(models.Model):
     """
     This is the user profile all users will have when the create an account 
@@ -12,14 +14,14 @@ class UserProfile(models.Model):
     tab
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=50, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = CountryField(blank_label='Country ', null=False, blank=False)
+    default_full_name = models.CharField(max_length=50, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country ', null=True, blank=False)
 
     def __str__(self):
         return self.user.username
