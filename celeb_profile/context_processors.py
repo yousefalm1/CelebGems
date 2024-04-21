401e4eaeabeb451b9aecfa8fccdd5cfe8be0801f
Yousefalm1 <yousefalm@outlook.com>
2024-04-21 17:01:07 +0300

    Fix request celeb profile in context proccesor and stlye of request for

--- celeb_profile/context_processors.py
+++ celeb_profile/context_processors.py
@@ -1,9 +1,10 @@
 from .models import CelebRequest
 
 def celeb_profile_status(request):
+    has_approved_request = False
+    
     if request.user.is_authenticated:
-        has_approved_request = True  
-    else:
-        has_approved_request = False
-
+        # Check if the user has an approved request for a celebrity profile
+        has_approved_request = CelebRequest.objects.filter(user=request.user, approved=True).exists()
+        
     return {'has_approved_request': has_approved_request}
