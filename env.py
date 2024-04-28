d317ab0e65716b6498426eb901b6af6b37ed9009
Yousefalm1 <yousefalm@outlook.com>
2024-04-28 23:07:23 +0300

    Fix env

--- env.py
+++ env.py
@@ -1,5 +1,4 @@
 import os
-
 os.environ["DATABASE_URL"]="postgres://nbzrzyvr:GhcwzvgIAW67YjF6oUB-A0XpgFNjl_jr@flora.db.elephantsql.com/nbzrzyvr"
 os.environ["SECRET_KEY"]="pml5jny5$(rulqtgdprax5!4u&2_-)r++)!ojb4+_p1)mye*qx"
 os.environ["CLOUDINARY_URL"]="cloudinary://675769358166127:kh040rA7PseSvEfOFcwIu2CyGVE@drjch7dpe"
