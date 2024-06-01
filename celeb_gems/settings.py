dee0605faecd287fe53b09721c0cabc9834e5006
Yousef Almesaeed <yousefalm@Outlook.com>
2024-06-01 19:39:04 +0300

    Remvoed email from settings

--- celeb_gems/settings.py
+++ celeb_gems/settings.py
@@ -181,10 +181,6 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
 
-    # EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-    # EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS', '')
-    # EMAIL_FROM = os.getenv('EMAIL_HOST_USER', '')
-
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    EMAIL_FROM = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS', '')
+    EMAIL_FROM = os.getenv('EMAIL_HOST_USER', '')
